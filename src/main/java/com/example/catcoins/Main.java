4f943177f7221f7bb388e1de2a36763e7a31898c
NunoGoncalve <1241621@isep.ipp.pt>
2025-05-21 01:46:44 +0100

    Ficheiros iniciais

---
+++ src/main/java/com/example/catcoins/Main.java
@@ -0,0 +1,30 @@
+package com.example.catcoins;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+            //StackPane root = loader.load();
+
+            Scene scene = new Scene(loader.load(), 1000, 1000);
+
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
