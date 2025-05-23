fe2d8b971881f48a51a3b82227faf45ab71bf2ad
Jose Perdomo <1241616@isep.ipp.pt>
2025-05-23 19:41:56 +0100

    Gerir Moedas

--- src/main/java/com/example/catcoins/Main.java
+++ src/main/java/com/example/catcoins/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("ManageCoin.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setTitle("Login");
         primaryStage.setScene(scene);
