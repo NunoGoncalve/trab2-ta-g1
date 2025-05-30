667759cdefc2ada2a1a2445baab8e5caf57c7a35
Jose Perdomo <1241616@isep.ipp.pt>
2025-05-30 20:38:08 +0100

    Listagem de Moedas e melhoras na ManageCoin

--- src/main/java/com/example/catcoins/Main.java
+++ src/main/java/com/example/catcoins/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("ManageCoin.fxml"));
         scene = new Scene(root);
         primaryStage.setTitle("Login");
         primaryStage.setScene(scene);
